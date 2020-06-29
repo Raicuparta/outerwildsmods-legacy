a7585cbc5fe7a5f7508be2ae8d5cf88744de9202
Ricardo Lopes <raicuparta@gmail.com>
2020-06-29 20:49:37 +0200

    Style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import utilStyles from '../styles/utils.module.css'
+import styles from '../components/layout.module.css';
 import { getSortedPostsData } from '../lib/posts'
 import { DownloadButton } from '../components/download-button'
 
@@ -10,16 +11,19 @@ type Props = {
 
 const Home: React.FunctionComponent<Props> = ({ allPostsData }) => {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Outer Wilds Mods</title>
       </Head>
+      <header className={styles.header}>
+        <h1 className={utilStyles.heading2Xl}>Outer Wilds Mods</h1>
+      </header>
       <section className={utilStyles.headingMd}>
         <DownloadButton>
           Download Outer Wilds Mod Manager
         </DownloadButton>
       </section>
-    </>
+    </div>
   )
 }
 
