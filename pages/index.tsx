c76af9ee98c3cdec38c25e4d80dba338382211ab
Ricardo Lopes <raicuparta@gmail.com>
2020-06-30 22:19:24 +0200

    Style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import styles from '../components/layout.module.css';
+import styles from '../styles/layout.module.css';
 import { DownloadButton } from '../components/download-button'
 import useModDatabase from '../hooks/useModDatabase';
 
@@ -17,21 +17,21 @@ const Home: React.FunctionComponent = () => {
       <header className={styles.header}>
         <h1 className={styles.pageTitle}>Outer Wilds Mods</h1>
       </header>
-      <h2 className={styles.sectionTitle}>Outer Wilds Mod Manager</h2>
-      <section className={styles.modManagerSection}>
-        <div className={styles.modManagerImageWrapper}>
-          <img className={styles.modManagerImage} src="images/mod-manager.png" />
+      <section className={styles.pageSection}>
+        <h2 className={styles.sectionTitle}>Outer Wilds Mod Manager</h2>
+        <div className={styles.sectionColumns}>
+          <div className={styles.sectionImageWrapper}>
+            <img className={styles.sectionImage} src="images/mod-manager.png" />
+          </div>
+          <div className={styles.sectionDescriptionWrapper}>
+            <p className={styles.sectionDescription}>
+              Use the Outer Wilds Mod Manager for downloading, installing, and managing mods.
+            </p>
+            <a href="https://github.com/Raicuparta/ow-mod-manager">
+              Source code on GitHub
+            </a>
+          </div>
         </div>
-        <div className={styles.modManagerDescriptionWrapper}>
-          <p className={styles.modManagerDescription}>
-            Use the Outer Wilds Mod Manager for downloading, installing, and managing mods.
-          </p>
-          <a href="https://github.com/Raicuparta/ow-mod-manager">
-            Source code on GitHub
-          </a>
-        </div>
-      </section>
-      <section>
         <DownloadButton
           href={isDatabaseLoaded ? modDatabase?.modManager.downloadUrl : '#'}
           disabled={!isDatabaseLoaded}
