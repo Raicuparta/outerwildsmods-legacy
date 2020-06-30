56d465a68621d14f25a8c40b83ba9a7c23f3b25c
Ricardo Lopes <raicuparta@gmail.com>
2020-06-30 22:03:24 +0200

    Mod manager info

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,21 @@ const Home: React.FunctionComponent = () => {
       <header className={styles.header}>
         <h1 className={utilStyles.heading2Xl}>Outer Wilds Mods</h1>
       </header>
-      <section className={utilStyles.headingMd}>
+      <h2 className={utilStyles.headingLg}>Outer Wilds Mod Manager</h2>
+      <section className={styles.modManagerSection}>
+        <div className={styles.modManagerImageWrapper}>
+          <img className={styles.modManagerImage} src="images/mod-manager.png" />
+        </div>
+        <div className={styles.modManagerDescriptionWrapper}>
+          <p className={styles.modManagerDescription}>
+            Use the Outer Wilds Mod Manager for downloading, installing, and managing mods.
+          </p>
+          <a href="https://github.com/Raicuparta/ow-mod-manager">
+            Source code on GitHub
+          </a>
+        </div>
+      </section>
+      <section>
         <DownloadButton
           href={isDatabaseLoaded ? modDatabase?.modManager.downloadUrl : '#'}
           disabled={!isDatabaseLoaded}
