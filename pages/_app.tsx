1de7a02d533018e0f7179045862dbf44a8846f90
Ricardo Lopes <raicuparta@gmail.com>
2020-08-01 11:24:10 +0200

    Restore analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,39 @@
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 import { useEffect } from 'react';
 import ReactGA from 'react-ga';
 
 import '../styles/global.css';
 import styles from '../styles/layout.module.scss';
+import { SmartLink } from '../components';
 
 const googleAnalyticsId = process.env.analyticsId;
 
+const Analytics: React.FunctionComponent<{ id?: string }> = ({ id }) => (
+  <>
+    <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${id}');
+        `,
+      }}
+    />
+  </>
+);
+
 export default function App({ Component, pageProps }: AppProps) {
-  const { asPath } = useRouter();
-
-  useEffect(() => {
-    if (googleAnalyticsId) {
-      ReactGA.initialize(googleAnalyticsId);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (googleAnalyticsId) {
-      ReactGA.pageview(asPath);
-    }
-  }, [asPath]);
-
   return (
     <>
+      <Analytics id={googleAnalyticsId} />
       <header className={styles.header}>
         <h1 className={styles.pageTitle}>
-          <Link href="/">
+          <SmartLink href="/">
             <a>Outer Wilds Mods</a>
-          </Link>
+          </SmartLink>
         </h1>
       </header>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,42 @@ import Document, {
   DocumentContext,
 } from 'next/document';
 
+function isPreloadLink(node: JSX.Element) {
+  return (
+    node && node.type === 'link' && node.props && node.props.rel === 'preload'
+  );
+}
+
+class CustomHead extends Head {
+  render() {
+    const res = super.render();
+
+    function transform(node: JSX.Element): JSX.Element {
+      if (isPreloadLink(node)) {
+        return <></>;
+      }
+      if (node && node.props && node.props.children) {
+        return {
+          ...node,
+          props: {
+            ...node.props,
+            children: Array.isArray(node.props.children)
+              ? node.props.children.map(transform)
+              : transform(node.props.children),
+          },
+        };
+      }
+      if (Array.isArray(node)) {
+        return <>{node.map(transform)}</>;
+      }
+
+      return node;
+    }
+
+    return transform(res);
+  }
+}
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -15,10 +51,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <CustomHead />
         <body>
           <Main />
-          <NextScript />
         </body>
       </Html>
     );
