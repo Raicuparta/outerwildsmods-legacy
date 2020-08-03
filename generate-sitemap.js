9eec45feed05387a97851c6ccfdcafa54c93a0b4
Ricardo Lopes <raicuparta@gmail.com>
2020-08-03 13:43:22 +0200

    Generate sitemap

---
+++ generate-sitemap.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const globby = require('globby');
+const prettier = require('prettier');
+
+(async () => {
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+
+  // Ignore Next.js specific files (e.g., _app.js) and API routes.
+  const pages = await globby([
+    'out/**/*.html',
+    '!out/404'
+  ]);
+
+  console.log(pages)
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages.map((page) => {
+    const path = page
+      .replace('out', '')
+      .replace('/index.html', '')
+      .replace('.html', '');
+
+    const route = path === '/index' ? '' : path;
+
+    return `
+          <url>
+            <loc>${`https://outerwildsmods.com${route}`}</loc>
+          </url>
+        `;
+  })
+      .join('')}
+      </urlset>
+  `;
+
+  // If you're not using Prettier, you can remove this.
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: 'html'
+  });
+
+  fs.writeFileSync('out/sitemap.xml', formatted);
+})();
