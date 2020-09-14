b62650ef76c20a4bb6b9dde5b9d2d4b0e564384b
Ricardo Lopes <raicuparta@gmail.com>
2020-09-14 23:37:36 +0200

    Cleanup images

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -5,9 +5,7 @@ import styles from './header.module.scss';
 export const Header = () => (
   <header className={styles.header}>
     <h1 className={styles.pageTitle}>
-      <SmartLink href="/">
-        <a>Outer Wilds Mods</a>
-      </SmartLink>
+      <SmartLink href="/">Outer Wilds Mods</SmartLink>
     </h1>
   </header>
 );
