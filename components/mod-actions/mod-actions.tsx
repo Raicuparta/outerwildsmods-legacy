5c89179c9da0e64bf4d2540e326f55e8fecc2617
Raicuparta <raicuparta@gmail.com>
2020-11-14 01:00:44 +0200

    Upgrade packages

--- components/mod-actions/mod-actions.tsx
+++ components/mod-actions/mod-actions.tsx
@@ -15,29 +15,33 @@ export const ModActions: React.FunctionComponent<Props> = ({
 }) => (
   <div className={`${styles.modActions} ${fullWidth ? styles.fullWidth : ''}`}>
     <div className={styles.content}>
-      <h1 className={styles.title}>{mod.manifest.name}</h1>
-        <TextLink isExternal href={mod.repo}>
-        by {mod.manifest.author}
-        </TextLink>
-      <p>{mod.manifest.description}</p>
       <div>
-        <small>
-          Downloaded {mod.downloadCount} times
-        </small>
+        <h1 className={styles.title}>{mod.manifest.name}</h1>
+          <TextLink isExternal href={mod.repo}>
+          by {mod.manifest.author}
+          </TextLink>
+        <p>{mod.manifest.description}</p>
+        <div>
+          <small>
+            Downloaded {mod.downloadCount} times
+          </small>
+        </div>
       </div>
-      <LinkButton
-        href="/mod-manager"
-        variant="primary"
-        className={styles.managerButton}
-      >
-        Install mod using Mod Manager
-      </LinkButton>
-      <LinkButton href={mod.downloadUrl}>
-        <small>
-          <div>Download mod files</div>
-          <div>Version {mod.manifest.version}</div>
-        </small>
-      </LinkButton>
+      <div className={styles.buttons}>
+        <LinkButton
+          href="/mod-manager"
+          variant="primary"
+          className={styles.managerButton}
+        >
+          Install mod using Mod Manager
+        </LinkButton>
+        <LinkButton href={mod.downloadUrl}>
+          <small>
+            <div>Download mod files</div>
+            <div>Version {mod.manifest.version}</div>
+          </small>
+        </LinkButton>
+      </div>
     </div>
   </div>
 );
