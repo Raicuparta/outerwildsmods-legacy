46435128f159362deab7ea9e7396991904fcec08
Raicuparta <raicuparta@gmail.com>
2020-11-14 20:33:28 +0200

    Cleanup, color adjustments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 
-import '../styles/global.css';
+import '../styles/global.scss';
 import { Footer, Header, Navigation } from '../components';
 
 export default function App({ Component, pageProps }: AppProps) {
