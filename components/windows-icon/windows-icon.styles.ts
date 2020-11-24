6b8f4f91c086bfa29e70ab4c997cef69d25c7371
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-24 23:00:28 +0100

    Style windows button with scss

---
+++ components/windows-icon/windows-icon.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { colors, spacing } from '../../styles/variables';
+
+export const Wrapper = styled.svg`
+  fill: ${colors.white};
+  margin-right: ${spacing.medium};
+`;
