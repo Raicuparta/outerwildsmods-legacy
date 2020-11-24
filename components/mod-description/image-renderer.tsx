dc5b6834e2ac4a9ccb1c98ae0f1a0e4142020832
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-24 22:26:38 +0100

    fixes for SEO and accessibility

--- components/mod-description/image-renderer.tsx
+++ components/mod-description/image-renderer.tsx
@@ -2,8 +2,9 @@ import { SmartImage } from '../smart-image';
 
 type Props = {
   src: string;
+  alt: string;
 };
 
-export const ImageRenderer: React.FunctionComponent<Props> = ({ src }) => (
-  <SmartImage src={src} height={4} width={30} layout="responsive" />
+export const ImageRenderer: React.FunctionComponent<Props> = ({ src, alt }) => (
+  <SmartImage alt={alt} src={src} height={4} width={30} layout="responsive" />
 );
