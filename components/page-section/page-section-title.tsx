3475b6fd2b7808b144b11d76ec14085a9f23bfdd
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-24 18:58:43 +0100

    Remove title space

--- components/page-section/page-section-title.tsx
+++ components/page-section/page-section-title.tsx
@@ -2,18 +2,14 @@ import { TitleWrapper, Title, Line } from './page-section.styles';
 
 type Props = {
   id: string;
-  spaced?: boolean;
 };
 
 export const PageSectionTitle: React.FunctionComponent<Props> = ({
   children,
   id,
-  spaced = false,
 }) => (
   <TitleWrapper>
-    <Title spaced={spaced} id={id}>
-      {children}
-    </Title>
+    <Title id={id}>{children}</Title>
     <Line />
   </TitleWrapper>
 );
