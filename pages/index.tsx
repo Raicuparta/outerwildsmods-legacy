68d5d1685190d27f92667b3c0a58dd1fc4cd1474
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-25 00:22:30 +0100

    Revert "Optimize images"
    This reverts commit 560f76987dd58e0f6a02ed00ea186325d8031790.

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const Home: React.FunctionComponent = () => (
     <PageSection title="Outer Wilds Mod Manager" id="mod-manager">
       <PageSectionColumns>
         <PageSectionImage
-          imageUrl="/images/mod-manager-small.png"
+          imageUrl="/images/mod-manager.png"
           title="Outer Wilds Mod Manager"
         />
         <PageSectionDescription description="Use the Outer Wilds Mod Manager for downloading, installing, and managing mods."></PageSectionDescription>
