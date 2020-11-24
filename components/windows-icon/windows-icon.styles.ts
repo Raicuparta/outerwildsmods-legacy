70e8d5711d722edbd86474b05cb4ce0771730214
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-25 00:22:30 +0100

    Revert "Style windows button with scss"
    This reverts commit 6b8f4f91c086bfa29e70ab4c997cef69d25c7371.

--- components/windows-icon/windows-icon.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-import { colors, spacing } from '../../styles/variables';
-
-export const Wrapper = styled.svg`
-  fill: ${colors.white};
-  margin-right: ${spacing.medium};
-`;
