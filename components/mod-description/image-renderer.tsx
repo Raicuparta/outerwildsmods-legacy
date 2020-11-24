46f1c354ad45184d9f81bc430f3fe40f3eaddd18
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-25 00:22:30 +0100

    Revert "Explicit smart image props"
    This reverts commit 129c884b3a7774dfb4a744f0bf3a9aff99ecf12b.

--- components/mod-description/image-renderer.tsx
+++ components/mod-description/image-renderer.tsx
@@ -1,9 +1,7 @@
 import { SmartImage } from '../smart-image';
 
-type Props = {
-  src: string;
-};
+type Props = Parameters<typeof SmartImage>[0];
 
-export const ImageRenderer: React.FunctionComponent<Props> = ({ src }) => (
-  <SmartImage src={src} height={4} width={30} layout="responsive" />
+export const ImageRenderer: React.FunctionComponent<Props> = (props) => (
+  <SmartImage {...props} height={4} width={30} layout="responsive" />
 );
