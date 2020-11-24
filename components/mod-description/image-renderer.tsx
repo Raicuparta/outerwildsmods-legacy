7b1c1ef672eed73d56f3d3d532a1a58075575be8
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-24 19:16:35 +0100

    Explicit smart image props

--- components/mod-description/image-renderer.tsx
+++ components/mod-description/image-renderer.tsx
@@ -1,7 +1,9 @@
 import { SmartImage } from '../smart-image';
 
-type Props = Parameters<typeof SmartImage>[0];
+type Props = {
+  src: string;
+};
 
-export const ImageRenderer: React.FunctionComponent<Props> = (props) => (
-  <SmartImage {...props} height={4} width={30} layout="responsive" />
+export const ImageRenderer: React.FunctionComponent<Props> = ({ src }) => (
+  <SmartImage src={src} height={4} width={30} layout="responsive" />
 );
