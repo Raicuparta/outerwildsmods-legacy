88d54e12c7486bdb763f12a6b0265cf228f194aa
Raicuparta <Raicuparta@users.noreply.github.com>
2020-11-28 06:58:58 +0100

    Defer analytics scripts

--- components/analytics/analytics.tsx
+++ components/analytics/analytics.tsx
@@ -4,13 +4,13 @@ export const Analytics: React.FunctionComponent<{ id: string }> = ({ id }) => {
   const isAmp = useAmp();
   return isAmp ? (
     <script
-      async
+      defer
       custom-element="amp-analytics"
       src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"
     />
   ) : (
     <>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
+      <script defer src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
       <script
         dangerouslySetInnerHTML={{
           __html: `
