a76040aff910517d6ee83990fa21e23b2f1eaf68
Raicuparta <raicuparta@gmail.com>
2021-11-23 00:51:14 +0100

    dynamic routes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,42 +9,6 @@ import { ServerStyleSheet } from 'styled-components';
 
 import { Analytics, AmpAnalytics } from '../components';
 
-function isPreloadLink(node: JSX.Element) {
-  return (
-    node && node.type === 'link' && node.props && node.props.rel === 'preload'
-  );
-}
-
-class CustomHead extends Head {
-  render() {
-    const res = super.render();
-
-    function transform(node: JSX.Element): JSX.Element {
-      if (isPreloadLink(node)) {
-        return <></>;
-      }
-      if (node && node.props && node.props.children) {
-        return {
-          ...node,
-          props: {
-            ...node.props,
-            children: Array.isArray(node.props.children)
-              ? node.props.children.map(transform)
-              : transform(node.props.children),
-          },
-        };
-      }
-      if (Array.isArray(node)) {
-        return <>{node.map(transform)}</>;
-      }
-
-      return node;
-    }
-
-    return transform(res);
-  }
-}
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
@@ -76,13 +40,13 @@ class MyDocument extends Document {
     const id = process.env.analyticsId ?? '';
     return (
       <Html lang="en">
-        <CustomHead>
+        <Head>
           <meta name="theme-color" content="#1f1f1f" />
           <Analytics id={id} />
-        </CustomHead>
+        </Head>
         <body>
           <Main />
-          {!process.env.isProd && <NextScript />}
+          <NextScript />
           <AmpAnalytics id={id} />
         </body>
       </Html>
